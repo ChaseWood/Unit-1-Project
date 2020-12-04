ad273484c5ba5e4840a61b13ff349f25abb3b03f
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-12-04 10:47:43 -0600

    adding php function to get emails from people

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php   if (isset($_POST['submit'
+])) {
+    $name = $_POST['name'];
+    $subject = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "rchasewood@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from" .$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
